4b3b2b2c48890b2e57a02c794f9e543ae210c741
rosaardila980 <rosaardila980@gmail.com>
2026-03-08 13:49:13 +0700

    first commit

---
+++ TPmod3_103022400022/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPmod3_103022400022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
